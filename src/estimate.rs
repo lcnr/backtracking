85adaf4ac23cd7c300ce4f57e3dd8568fd7809bb
lcnr/Bastian Kauschke <bastian_kauschke@hotmail.de>
2019-08-11 10:54:02 +0200

    improve docs

--- src/estimate.rs
+++ src/estimate.rs
@@ -4,14 +4,19 @@ use rand::{seq::IteratorRandom, thread_rng};
 
 use std::time::{Duration, Instant};
 
+/// Tries to build a random sequence of length `n` without any backtracking,
+/// returns the estimated time actual backtracking would take for this sequence.
+///
+/// This function only tries 1 path, so the results are highly unstable and might
+/// not always be representative. For a better estimate use `repeated_estimate`
+/// with many `repetitions`.
 pub fn e<T: Sequence>(initial: T, n: usize) -> Duration {
     let mut current = initial;
 
     let mut estimated_steps = 1;
     let mut total_length = Duration::from_secs(0);
 
-    let mut depth = 0;
-    while depth < n {
+    for _ in 0..n - 1 {
         let now = Instant::now();
         let mut next_states = current.next_states();
         next_states.retain(|state| state.satisfies_condition());
@@ -19,7 +24,6 @@ pub fn e<T: Sequence>(initial: T, n: usize) -> Duration {
 
         estimated_steps *= next_states.len() as u32;
         if let Some(next) = next_states.into_iter().choose(&mut thread_rng()) {
-            depth += 1;
             current = next;
         } else {
             break;
@@ -29,6 +33,8 @@ pub fn e<T: Sequence>(initial: T, n: usize) -> Duration {
     total_length
 }
 
+/// tries to estimate the total time needed to solve the given backtracking problem
+/// by repeatedly calling `e` and calculating the average
 pub fn repeated_estimate<T: Sequence + Clone>(initial: T, n: usize, repetitions: u32) -> Duration {
     let mut total_duration = Duration::from_secs(0);
 
